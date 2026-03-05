c34dc2e75ad2907d1de98a7fdd61895d3e350e6e
bgcutku <bgcutku34@gmail.com>
2026-03-06 01:16:17 +0300

    Admin Dashboard toplam ilan, satılık, kiralık, toplam mesaj eklendi.

--- EmlakAlimSatim/Areas/Admin/Controllers/DashboardController.cs
+++ EmlakAlimSatim/Areas/Admin/Controllers/DashboardController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using EmlakAlimSatim.Filters;
+using EmlakAlimSatim.Data;
+using EmlakAlimSatim.Areas.Admin.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmlakAlimSatim.Areas.Admin.Controllers
 {
@@ -7,9 +10,36 @@ namespace EmlakAlimSatim.Areas.Admin.Controllers
     [AdminAuthFilter]  // Admin paneli koruma
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly EmlakDbContext _context;
+        public DashboardController(EmlakDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var mod = new DashboardViewModel
+            {
+                //Kart Verileri
+                TotalPropertiesCount = await _context.Properties.CountAsync(),
+                SaleCount = await _context.Properties.CountAsync(m => m.ListingType == "Satılık"),
+                RentCount = await _context.Properties.CountAsync(m => m.ListingType == "Kiralık"),
+                TotalMessagesCount = await _context.Messages.CountAsync(),
+
+                //Son 5 Eklenen İlanlar
+                RecentProperties = await _context.Properties
+                    .OrderByDescending(m => m.Id)
+                    .Take(5)
+                    .ToListAsync(),
+
+                //Son 5 Mesaj
+                RecentMessages = await _context.Messages
+                    .OrderByDescending(m => m.Id)
+                    .Take(5)
+                    .ToListAsync()
+
+
+            };
+            return View(mod);
         }
     }
 }
---
+++ EmlakAlimSatim/Areas/Admin/Models/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using EmlakAlimSatim.Models;
+
+namespace EmlakAlimSatim.Areas.Admin.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalPropertiesCount { get; set; }
+        public int SaleCount { get; set; }
+        public int RentCount { get; set; }
+        public int TotalMessagesCount { get; set; }
+
+        public List<Property> RecentProperties { get; set; }
+        public List<Message> RecentMessages { get; set; }
+
+    }
+}
